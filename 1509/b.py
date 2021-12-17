d8936bf6e69dfbbc07835691cb453fb8e0a14756
annieFromTaiwan <fewwordsmanydeeds@gmail.com>
2021-12-17 22:56:48 +0800

    1509.b

---
+++ 1509/b.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        """
+        nums = [1,2,3,4,5,6,7,8,9,10]
+                ^ ^ ^       
+                ^ ^                ^
+                ^               ^  ^
+                              ^ ^  ^
+        """
+        if len(nums) <= 4:
+            return 0
+        nums.sort()
+        return min(
+            nums[-1] - nums[3],
+            nums[-2] - nums[2],
+            nums[-3] - nums[1],
+            nums[-4] - nums[0]
+        )
